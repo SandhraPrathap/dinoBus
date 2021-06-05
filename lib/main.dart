7c8305d5a43bc64c849a1aec82389b5c4cb1610d
Aloysious Benoy <aloysiousbenoy@gmail.com>
2021-06-05 22:08:37 +0530

    store widget state as private membr

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-int n=0;
-void add()
-{
+
+int n = 0;
+void add() {
   n++;
 }
 
@@ -17,6 +17,14 @@ class DinoBus extends StatefulWidget {
 }
 
 class _DinoBusState extends State<DinoBus> {
+  int _n = 0;
+
+  void _add() {
+    setState(() {
+      n++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,77 +40,83 @@ class _DinoBusState extends State<DinoBus> {
               Image.asset("image/bus1.png")
             ],
           )),
-      body: 
-          Stack(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        stops: [0.10, 1],
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Color.fromRGBO(17, 16, 16, 1),
-                          Color.fromRGBO(249, 224, 5, 1)
-                        ])),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    stops: [0.10, 1],
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color.fromRGBO(17, 16, 16, 1),
+                      Color.fromRGBO(249, 224, 5, 1)
+                    ])),
+          ),
+          Positioned(
+            bottom: MediaQuery.of(context).size.height * 0.1,
+            left: MediaQuery.of(context).size.width * 0.1,
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.black,
               ),
-              Positioned(
-                  bottom: MediaQuery.of(context).size.height * 0.1,
-                  left: MediaQuery.of(context).size.width * 0.1,
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.black,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  primary: Color.fromRGBO(249, 224, 5, 1),
+                ),
+                onPressed: () {},
+                child: Align(
+                    alignment: Alignment.center,
+                    child: Center(child: Image.asset("image/bus21.png"))),
+              ),
+            ),
+          ),
+          Positioned(
+              bottom: MediaQuery.of(context).size.height * 0.1,
+              right: MediaQuery.of(context).size.width * 0.1,
+              child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black,
+                  ),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      primary: Color.fromRGBO(249, 224, 5, 1),
                     ),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        primary: Color.fromRGBO(249, 224, 5, 1),
-                      ),
-                      onPressed: () {},
-                      child:Align(alignment: Alignment.center,child: Center(child: Image.asset("image/bus21.png"))),
-                    ),
-                  ),),
-              Positioned(
-                  bottom: MediaQuery.of(context).size.height * 0.1,
-                  right: MediaQuery.of(context).size.width * 0.1,
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.black,
-                    ),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        primary: Color.fromRGBO(249, 224, 5, 1),
-                      ),
-                      onPressed: () {add;},
-                      child: Align(alignment: Alignment.center,child:Text("+",textAlign: TextAlign.center,
+                    onPressed: () {
+                      add;
+                    },
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("+",
+                          textAlign: TextAlign.center,
                           style: TextStyle(
                               fontSize: 75,
                               color: Color.fromRGBO(249, 224, 5, 1),
                               fontWeight: FontWeight.bold)),
-                    ),)
-                  )),
-              Positioned(
-                top: MediaQuery.of(context).size.height * 0.20,
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Text('$n',
-                    style: TextStyle(
-                        fontSize: 200,
-                        color: Colors.black,
-                        fontWeight: FontWeight.w300),
-                    textAlign: TextAlign.center),
-              )
-            ],
-          ),
-      
+                    ),
+                  ))),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.20,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Text('$n',
+                style: TextStyle(
+                    fontSize: 200,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w300),
+                textAlign: TextAlign.center),
+          )
+        ],
+      ),
     );
   }
 }
