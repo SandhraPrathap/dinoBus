0ced7f70110daed9871d5063f0086da98682cf40
Sandhra <sandhraprathap@gmail.com>
2021-06-07 22:18:30 +0530

    blah

--- lib/main.dart
+++ lib/main.dart
@@ -13,33 +13,30 @@ class DinoBus extends StatefulWidget {
 
 class _DinoBusState extends State<DinoBus> {
   int _n = 0;
-  
-  
+
   bool _showBus = false;
-  bool _gameOver=false;
+  bool _gameOver = false;
 
   void _add() {
-    
-      _showBus = false;
-      _n++;
-      if(_n%5==0)
-      {_gameOver=true;}
-      setState(() {});
-      
-      
-    
+    _showBus = false;
+    _n++;
+    if (_n % 5 == 0) {
+      _gameOver = true;
+    }
+    setState(() {});
   }
 
   void _bus() {
-    _n++;
-    
+   _n++;
+
     setState(() {
       _showBus = true;
+      
     });
-    
-    if(_n%5!=0)
-      {_gameOver=true;}
 
+    if (_n % 5 != 0) {
+      _gameOver = true;
+    }
   }
 
   @override
@@ -58,91 +55,116 @@ class _DinoBusState extends State<DinoBus> {
             ],
           )),
       body: Stack(
-        children:_gameOver?(<Widget>[Container(color:Colors.red,height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,child:Center(child: Text("Game Over",style: TextStyle(fontSize: 34,fontWeight: FontWeight.bold),)))]):( <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    stops: [0.10, 1],
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color.fromRGBO(17, 16, 16, 1),
-                      Color.fromRGBO(249, 224, 5, 1)
-                    ])),
-          ),
-          Positioned(
-            bottom: MediaQuery.of(context).size.height * 0.1,
-            left: MediaQuery.of(context).size.width * 0.1,
-            child: Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.black,
-              ),
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  primary: Color.fromRGBO(249, 224, 5, 1),
-                ),
-                onPressed: () {
-                  _bus();
-                },
-                child: Align(
-                    alignment: Alignment.center,
-                    child: Center(child: Image.asset("image/bus21.png"))),
-              ),
-            ),
-          ),
-          Positioned(
-              bottom: MediaQuery.of(context).size.height * 0.1,
-              right: MediaQuery.of(context).size.width * 0.1,
-              child: Container(
-                  width: 100,
-                  height: 100,
+        children: _gameOver
+            ? (<Widget>[
+                Container(
+                    color: Colors.red,
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(
+                        child: Text(
+                      "Game Over",
+                      style:
+                          TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+                    )))
+              ])
+            : (<Widget>[
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.black,
-                  ),
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      primary: Color.fromRGBO(249, 224, 5, 1),
-                    ),
-                    onPressed: () {
-                      _add();
-                    },
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text("+",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 75,
-                              color: Color.fromRGBO(249, 224, 5, 1),
-                              fontWeight: FontWeight.bold)),
-                    ),
-                  ))),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.20,
-            left: 0,
-            right: 0,
-            child: _showBus
-                ? Container(height: MediaQuery.of(context).size.height * 0.3,
-                
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          stops: [0.10, 1],
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Color.fromRGBO(17, 16, 16, 1),
+                            Color.fromRGBO(249, 224, 5, 1)
+                          ])),
+                ),
+                Positioned(
+                  bottom: MediaQuery.of(context).size.height * 0.1,
+                  left: MediaQuery.of(context).size.width * 0.1,
+                  child: Container(
+                    width: 100,
+                    height: 100,
                     decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                        image: DecorationImage(
-                          fit:BoxFit.fitHeight,
-                            image: AssetImage("image/bus10.png"))))
-                : (Text('$_n',
-                    style: TextStyle(
-                        fontSize: 200,
-                        color: Colors.black,
-                        fontWeight: FontWeight.w300),
-                    textAlign: TextAlign.center)),
-          )
-        ]),
+                      shape: BoxShape.circle,
+                      color: Colors.black,
+                    ),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        primary: Color.fromRGBO(249, 224, 5, 1),
+                      ),
+                      onPressed: () {
+                        _bus();
+                         Future.delayed(Duration(milliseconds:500), () {
+
+                              if (_n % 5 == 0)
+                                _add();
+                              
+                            });
+                      },
+                      child: Align(
+                          alignment: Alignment.center,
+                          child: Center(child: Image.asset("image/bus21.png"))),
+                    ),
+                  ),
+                ),
+                Positioned(
+                    bottom: MediaQuery.of(context).size.height * 0.1,
+                    right: MediaQuery.of(context).size.width * 0.1,
+                    child: Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.black,
+                        ),
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            primary: Color.fromRGBO(249, 224, 5, 1),
+                          ),
+                          onPressed: () {
+                            _add();
+                            Future.delayed(Duration(milliseconds:500), () {
+                              if((_n+1)%5==0)
+                              _bus();
+                              else if (_n % 5 != 0)
+                                {_add();}
+                              
+                            });
+                          },
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text("+",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 75,
+                                    color: Color.fromRGBO(249, 224, 5, 1),
+                                    fontWeight: FontWeight.bold)),
+                          ),
+                        ))),
+                Positioned(
+                  top: MediaQuery.of(context).size.height * 0.20,
+                  left: 0,
+                  right: 0,
+                  child: _showBus
+                      ? Container(
+                          height: MediaQuery.of(context).size.height * 0.3,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              image: DecorationImage(
+                                  fit: BoxFit.fitHeight,
+                                  image: AssetImage("image/bus10.png"))))
+                      : (Text('$_n',
+                          style: TextStyle(
+                              fontSize: 200,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w300),
+                          textAlign: TextAlign.center)),
+                )
+              ]),
       ),
     );
   }
